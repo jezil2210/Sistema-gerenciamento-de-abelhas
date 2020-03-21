151e05261f8a6ad3e106851e868d4be58c4d3bb7
Jeziel Suzana <60527278+jezil2210@users.noreply.github.com>
2020-03-21 10:00:55 -0300

    adding conectando_site.ino

---
+++ conectando_site.ino
@@ -0,0 +1,96 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiServer.h>
+//#include <NTPClient.h>
+#include "DHTesp.h"
+
+#define DHTpin 23
+
+const char* ssid = "labores";
+const char* password = "labores2019";
+
+const char* host = "www.abelhascefet.tech";
+
+/*const char* servidorNTP = "a.st1.ntp.br"; // Servidor NTP para pesquisar a hora
+ 
+const int fusoHorario = -10800; // Fuso horário em segundos (-03h = -10800 seg)
+const int taxaDeAtualizacao = 1800000; // Taxa de atualização do servidor NTP em milisegundos
+
+WiFiUDP ntpUDP; // Declaração do Protocolo UDP
+NTPClient timeClient(ntpUDP, servidorNTP, fusoHorario, 60000);
+
+*/
+DHTesp dht;
+WiFiClient client;
+
+void setup()
+{
+  Serial.begin(115200);
+  //timeClient.begin();
+  dht.setup(DHTpin, DHTesp::DHT11);
+  Serial.printf("Connecting to %s ", ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" connected");
+
+}
+
+
+void loop()
+{  
+    delay(20000);
+  //timeClient.update();
+
+  float humid= dht.getHumidity();
+  float temp = dht.getTemperature();
+  //String horario = timeClient.getFormattedTime();
+
+  if (isnan(humid) || isnan(temp)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+  }
+                                                                
+  Serial.print("Humidade: ");
+  Serial.print(humid);
+  Serial.print("%  Temperatura: ");
+  Serial.print(temp);
+  //Serial.println(horario);
+
+  Serial.printf("\n[Connecting to %s ... ", host);
+  
+  if (client.connect(host, 80))
+  {
+    Serial.println("connected]");
+    
+    String param = "?temp=" + String(temp) + "&humid=" + String(humid);
+
+    Serial.println("[Sending a request]");
+    Serial.println(param);
+    client.print(String("GET /abelhas.php")+ param + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n" +
+                 "\r\n"
+                );
+
+    Serial.println("[Response:]");
+    while (client.connected() || client.available())
+    {
+      if (client.available())
+      {
+        String line = client.readStringUntil('\n');
+        Serial.println(line);
+      }
+    }
+    client.stop();
+    Serial.println("\n[Disconnected]");
+  }
+  else
+  {
+    Serial.println("connection failed!]");
+    client.stop();
+  }
+  
+}
